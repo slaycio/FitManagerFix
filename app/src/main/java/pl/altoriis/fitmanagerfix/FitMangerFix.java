66ae07c39c1bc66449ef9433b888508fc4db4268
p <p>
2015-02-15 21:52:08 +0100

    dobilismy sie do dashboarda i moze to jest OK

--- app/src/main/java/pl/altoriis/fitmanagerfix/FitMangerFix.java
+++ app/src/main/java/pl/altoriis/fitmanagerfix/FitMangerFix.java
@@ -1,26 +1,27 @@
 package pl.altoriis.fitmanagerfix;
 
-import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XC_MethodReplacement;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import android.content.Context;
+import android.os.Message;
 
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
-/**
- * Created by p on 2015-01-25.
- */
-// SM-900S - device
-//SM-G313F
-    // MODEL GT-I9505
-    //
+import com.sec.android.app.shealthlite.datamanager.SHealthLiteSyncManager;
 
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+
+    //SM-G313F
 
     // to wyglada na jakas funcke kjtra mozna uzyc
-    //ACTION = com.sec.android.app.shealthlite.syncstartfullysyncrefresh
+    //com.sec.android.app.shealthlite.dashboard.SHealthLiteDashboard
+    //onServiceConnected
 
 public class FitMangerFix implements IXposedHookLoadPackage {
 
@@ -32,82 +33,29 @@ public class FitMangerFix implements IXposedHookLoadPackage {
             XposedBridge.log("package: " + lpparam.packageName);
         }
 
-        if (lpparam.packageName.equals("com.samsung.android.wms")) {
-            XposedBridge.log("com.samsung.android.wms inside");
-
-            //szukanie
-            String[][] lista = new String[1][2];
-
-            lista[0][0] = "com.samsung.android.wms.app.ClocksFragment";
-            lista[0][1] = "onResume";
-
-/*
-            lista[1][0] = "com.samsung.android.wms.app.PairedWingtipFragment";
-            lista[1][1] = "onClick";
-            lista[2][0] = "com.samsung.android.wms.app.PairedWingtipFragment";
-            lista[2][1] = "onActivityResult";
-            lista[3][0] = "com.samsung.android.wms.app.PairedWingtipFragment";
-            lista[3][1] = "onCreate";
-            lista[4][0] = "com.samsung.android.wms.app.ClocksFragment";
-            lista[4][1] = "onItemClick";
-*/
+        if (!lpparam.packageName.equals("com.sec.android.app.shealthlite"))
+            return;
 
 
-            for(final String[] x : lista) {
-
-/*
-                try {
-                    XposedBridge.log("proba" + x[0] + ":::" + x[1]);
-
-                    findAndHookMethod(x[0], lpparam.classLoader, x[1],
-                            XC_MethodReplacement.DO_NOTHING);
-                } catch (Throwable t) {
-                    XposedBridge.log(t);
-                }
-*/
-
-
-                try {
-
-                    XposedBridge.log("przed zmiana metody");
-
-
-                    findAndHookMethod(x[0], lpparam.classLoader, x[1], new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            // this will be called before the clock was updated by the original method
-                            XposedBridge.log(x[0]+ "przed");
-                        }
-
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            // this will be called after the clock was updated by the original method
-                            XposedBridge.log(x[0]+" po");
-                        }
-                    });
-
-                    XposedBridge.log("po zmiana metody");
-                } catch (Throwable t) {
-                    XposedBridge.log("wywalilisie");
-                    XposedBridge.log(t);
-                }
-
-            }
-
-
-        }
-
-        if (lpparam.packageName.equals("com.sec.android.app.shealthlite")) {
             XposedBridge.log("com.sec.android.app.shealthlite inside");
 
             //szukanie
             String[][] lista = new String[1][2];
 
-            lista[0][0] = "com.sec.android.app.shealthlite.SHealthLiteApplication";
-            lista[0][1] = "onCreate";
+            lista[0][0] = "com.sec.android.app.shealthlite.dashboard.SHealthLiteDashboard";
+            lista[0][1] = "action_sync_now";
 
             for(final String[] x : lista) {
 
+                boolean bSHealthLiteDashboardExists = false;
+                try {
+
+                    bSHealthLiteDashboardExists = XposedHelpers.findClass("com.sec.android.app.shealthlite.dashboard.SHealthLiteDashboard"
+                            , lpparam.classLoader) == null ? false : true;
+                } catch (Exception e) {
+                    XposedBridge.log("nie ma klasy nie ma");
+
+                } 
 /*
                 try {
                     XposedBridge.log("proba" + x[0] + ":::" + x[1]);
@@ -119,37 +67,48 @@ public class FitMangerFix implements IXposedHookLoadPackage {
                 }
 */
 
+                if (bSHealthLiteDashboardExists) {
+                    XposedBridge.log("bSHealthLiteDashboardExists");
+                    try {
 
-                try {
-
-                    XposedBridge.log("przed zmiana metody");
+                        XposedBridge.log("a teraz TRY");
 
 
-                    findAndHookMethod(x[0], lpparam.classLoader, x[1], new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            // this will be called before the clock was updated by the original method
-                            XposedBridge.log(x[0]+ "przed");
+                        findAndHookMethod(x[0], lpparam.classLoader, x[1], new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                                // this will be called before the clock was updated by the original method
+                                XposedBridge.log(x[0] + "przed");
 
-                        }
+                            }
 
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            // this will be called after the clock was updated by the original method
-                            XposedBridge.log(x[0]+" po");
-                        }
-                    });
+                            @Override
+                            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                // this will be called after the clock was updated by the original method
 
-                    XposedBridge.log("po zmiana metody");
-                } catch (Throwable t) {
-                    XposedBridge.log("wywalilisie");
-                    XposedBridge.log(t);
+
+                                SHealthLiteSyncManager mProgress = (SHealthLiteSyncManager) getObjectField(param.thisObject, "mProgress");
+
+                                Object state = XposedHelpers.callMethod(mCM, "getState");
+
+
+
+                                XposedBridge.log(x[0] + " po");
+                            }
+                        });
+
+                        XposedBridge.log("po zmiana metody");
+                    } catch (Throwable t) {
+                        XposedBridge.log("wywalilisie");
+                        XposedBridge.log(t);
+                    }
+
                 }
 
 
 
 
-            }
+
 
 
         }
